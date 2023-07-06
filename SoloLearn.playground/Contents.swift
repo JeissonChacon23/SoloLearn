6706dc16b4bf1fc52bdc7bbe9cdda3b8ac1744d2
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-06 15:43:06 -0500

    Type annotations

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,12 +1,23 @@
 import Foundation
 
-//var is used to declare a variable
-//let is used to declare a constant that after its value was asigned cannot be changed
+//Int
+var x:Int = 23
+var y = 23
 
-var num:Int = 4
-num = 5
-print(num)
+//Double
+var m:Double = 3.0
+var n = 3.0
+var red, blue, green:Double
 
-let pi:Double = 3.1416
-//pi = 3.00000 --> Error
-print(pi)
+//Float
+var a:Float = 4.0
+
+//Bool
+var young:Bool = true
+young = false
+print(young)
+
+//String
+var name:String = "Jeisson"
+var lastName = "Chacon"
+print(name, lastName)
