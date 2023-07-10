24b0f765b689fb1af4a225d67432b98b843866ca
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-10 14:26:14 -0500

    Optionals

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,13 +1,6 @@
 import Foundation
 
-//Range Operators
-//1...3  --> 1,2,3
-//1..<3  --> 1,2
-
-//Logical Operators
-var state = true
-!state
-var a = false
-var b = true
-state = a && b
-state = a || b
+//Optionals
+var myCode:Int? = 404
+myCode = nil
+var someMsg: String?
