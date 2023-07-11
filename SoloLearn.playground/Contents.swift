7edcc4455fae8ff7a8d41e3ee7e616a9bb31ea0f
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-11 11:04:41 -0500

    For

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,22 +1,6 @@
 import Foundation
 
-//While
-var a = 1
-var b = 10
-while a < b{
-    print(a)
-    a += 1
+//For
+for index in 1...5{
+    print(index)
 }
-
-//Repeat while
-var x = 5
-repeat{
-    print(x)
-    x -= 1
-}while x > 0
-
-var i = 1, n = 3
-repeat {
-   print(i)
-   i+=1
-} while i < n
