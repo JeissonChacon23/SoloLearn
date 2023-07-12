b1f34e63ebb9f07f81b0b8b70bd6d8c70ea6417a
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-11 19:33:25 -0500

    Continue, break and Fallthrought

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,6 +1,45 @@
 import Foundation
 
-//For
-for index in 1...5{
-    print(index)
+//Control transfer
+//Continue
+for num in 1...5{
+    if num % 2 == 0{
+        continue
+    }
+    print(num)
 }
+print("*********")
+//Break
+var b = 7
+var a = 10
+while a > 0 {
+   if(a < b) {
+     break
+   }
+   print(a)
+   a-=1
+}
+
+var num = 5
+var letter = "X"
+switch a{
+case 1:
+    print("A")
+case 2:
+    print("B")
+default:
+    break
+}
+print(letter)
+
+//Fallthrought
+let myInt = 5
+var desc = "The number \(myInt) is"
+switch myInt {
+   case 2, 3, 5, 7, 11, 13, 17, 19:
+      desc += " a prime number, and also"
+      fallthrough
+   default:
+     desc += " an integer."
+}
+print(desc)
