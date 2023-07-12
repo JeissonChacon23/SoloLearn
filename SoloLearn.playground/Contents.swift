c79e9f5c65d2f3bc1e909b2103daa17ffa7a1be5
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-11 23:10:59 -0500

    Strings

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,45 +1,32 @@
 import Foundation
 
-//Control transfer
-//Continue
-for num in 1...5{
-    if num % 2 == 0{
-        continue
-    }
-    print(num)
-}
-print("*********")
-//Break
-var b = 7
-var a = 10
-while a > 0 {
-   if(a < b) {
-     break
-   }
-   print(a)
-   a-=1
+//Strings
+var someString = "Hello, World!"
+var myStrign = String() //This is an empty Strign
+var anotherEmptyString = ""
+if anotherEmptyString.isEmpty{
+    print("Is empty")
 }
 
-var num = 5
-var letter = "X"
-switch a{
-case 1:
-    print("A")
-case 2:
-    print("B")
-default:
-    break
-}
-print(letter)
+//Concatenation
+let string1 = "Hello"
+let string2 = " World"
+print(string1 + string2)
 
-//Fallthrought
-let myInt = 5
-var desc = "The number \(myInt) is"
-switch myInt {
-   case 2, 3, 5, 7, 11, 13, 17, 19:
-      desc += " a prime number, and also"
-      fallthrough
-   default:
-     desc += " an integer."
+var msg = "Hello"
+msg += " World"
+
+//Interpolation
+let mult = 4
+let message = "\(mult) times 1.5 is \(Double(mult) * 1.5)"
+print(message)
+
+//Counting characters
+print(message.count)
+
+//Comparing Strings
+if string1 == string2{
+    print("Equals")
+} else{
+    print("Aren't equals")
 }
-print(desc)
