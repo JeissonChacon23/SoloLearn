3f6250ff5652e493556c351b4ff060ed95b15cca
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-13 14:51:07 -0500

    Set Operations

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,34 +1,17 @@
 import Foundation
 
-//Set
-var letters = Set<Character>()
-print(letters)
+//Set opetations
+let oddDigits: Set = [1, 3, 5, 7, 9]
+let evenDigits: Set = [0, 2, 4, 6, 8]
 
-var names: Set<String> = ["David", "Susan", "Robert"]
-//var names: Set = ["David", "Susan", "Robert"]
+print("Set 1 contains \(oddDigits)")
+print("Set 2 contains \(evenDigits)")
+print(oddDigits.union(evenDigits).sorted())
+print(oddDigits.intersection(evenDigits).sorted())
+print(oddDigits.symmetricDifference(evenDigits).sorted())
+print(oddDigits.subtracting(evenDigits).sorted())
 
-//Insert
-names.insert("Paul")
-print(names)
-
-//Contains
-if names.contains("David"){
-    print("David is here.")
-} else{
-    print("David isn't with us.")
-}
-
-//Iterating over a Set
-for name in names{
-    print(name)
-}
-for name in names.sorted(){
-    print(name)
-}
-
-//Remove
-names.remove("Paul")
-names.remove("Susan")
-print(names)
-names.removeAll()
-print(names)
+let a: Set = ["A", "B", "C"]
+var b: Set = ["B", "D", "E", "A"]
+print(b.subtracting(a))
+print(b.subtracting(a).count)
