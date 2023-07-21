76c5bb346619881b496a3f842f1cd1f2a2d2ff67
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-07-21 13:14:15 -0500

    Functions

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,28 +1,47 @@
 import Foundation
 
-//Dictionaries
-//var airport = [Int: String]()
-//var airports: [String: String] = ["TOR":  "Toronto", "NY": "New York"]
-//var persona: [Int: String] = [1094277015: "Jeisson Chacon"]
-
-var airports = ["TOR":  "Toronto", "NY": "New York"]
-airports["LHR"] = "London"
-print(airports)
-airports["LHR"] = "London Heathrow"
-print(airports)
-let oldValue = airports.updateValue("New York Airport", forKey: "NY")
-print(airports)
-let airportName = airports["NY"]
-print(airportName)
-airports["LHR"] = "London"
-airports["APL"] = "Apple"
-airports["APL"] = nil
-print(airports)
-if let removedValue = airports.removeValue(forKey: "NY") {
-   print("The removed airport's name is \(removedValue).")
-} else {
-   print("The airports dictionary does not contain a value for NY.")
+//Functions
+func sayHello(name: String) -> String{
+    let greeting = "Hello, " + name
+    return greeting
 }
-var test = [1:"A", 2:"B", 3:"C"]
-test[3] = nil
-print(test.count)
+
+print(sayHello(name: "Chacon"))
+
+func rangeLenght(start: Int, end: Int) -> Int{
+    return end - start
+}
+print(rangeLenght(start: 2, end: 7))
+
+func sayHelloWorld() ->String{
+    return "Hello, World!"
+}
+print(sayHelloWorld())
+
+func sayHi(name: String){
+    print("Hi! \(name)")
+}
+sayHi(name: "Jeisson")
+
+func minMax(array: [Int]) -> (min: Int, max: Int){
+    var min = array[0]
+    var max = array[0]
+    for value in array[1 ..< array.count]{
+        if value < min{
+            min = value
+        }else if value > max{
+            max = value
+        }
+    }
+    return (min, max)
+}
+print(minMax(array: [1, 2, 3, 4, -6, 11]))
+let bounds = minMax(array: [4, -4, 1, 88, 7, 42])
+bounds.min
+bounds.max
+
+func test(n1: Int, n2:Int) -> (a: Int, b: Int) {
+   return ((n1-n2), (n1+n2))
+}
+let tmp = test(n1: 8, n2: 3)
+print(tmp.b)
