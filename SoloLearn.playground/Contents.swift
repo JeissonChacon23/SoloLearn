52c7128b81a3f430f457d811469fbd4d96a8dd0b
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-27 09:00:31 -0500

    Tuples

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,36 +1,19 @@
 import Foundation
 
-//The sorted function
-func backwards(s1: String, s2: String) -> Bool{
-    return s1 > s2
-}
+//Tuples
+let error = (404, "No found")
+print(error)
+let (statusCode, statusMessage) = error
+print(statusCode)
+print(statusMessage)
 
-let names = ["Cc", "Aa", "Ee", "Bb", "Dd"]
-print(names)
-/*var reversed = names.sorted(by: backwards)
-print(reversed)*/
-var reversed = names.sorted(by: { (s1: String, s2: String) -> Bool in
-    return s1 > s2
-})
-print(reversed)
+print(error.0)
+print(error.1)
 
-var asc = names.sorted(by: { (s1: String, s2: String) -> Bool in
-    return s1 < s2
-})
-print(asc)
-//Infering type from context
-var otherReversed = names.sorted(by: { s1, s2 in return s1 > s2 } )
-print(reversed)
+let http200Status = (statusCode: 200, description: "OK")
+print(http200Status.statusCode)
+print(http200Status.description)
 
-var reversedNew = names.sorted(by: { s1, s2 in s1 > s2 } )
-print(reversedNew)
-var reversedOneMore = names.sorted(by: { $0 > $1 } )
-print(reversedOneMore)
-
-//Operator functions
-var reversedShorterForm = names.sorted(by: >)
-print(reversedShorterForm)
-
-let letters = ["c", "a", "e", "b"]
-let res = letters.sorted(by: <)
-print(res[0])
+let coords = (48, 27)
+let latitude = coords.0
+let longitude = coords.1
