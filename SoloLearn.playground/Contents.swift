4573862207d9173b6ef31d0bac8b447dcb3f6135
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-29 09:42:26 -0500

    Struct and Class

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -34,3 +34,42 @@ print(someVideoMode.resolution.height)
 let vga = Resolution(width: 640, height: 480)
 let mySize = Size()
 print(mySize.width)
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+print(hd)
+
+cinema.width = 2048
+
+let res = Resolution()
+print(res.width)
+
+class VideoMode {
+   var resolution = Resolution()
+   var interlaced = false
+   var frameRate = 0.0
+}
+
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+print(tenEighty.resolution)
+
+let alsoTenEighty = tenEighty
+alsoTenEighty.interlaced = false
+print(alsoTenEighty.interlaced)
+
+
+class Test {
+   var num = 17
+}
+let a = Test()
+let b = a
+b.num = 42
+print(a.num)
+
+if tenEighty === alsoTenEighty {
+ print("Same")
+} else {
+ print("Not same")
+}
