9a5589464c430595baed20962f75270000a21e0a
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-07-29 10:12:11 -0500

    Differences between Struct and Class

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,75 +1,32 @@
 import Foundation
 
-//Object Oriented Programming
-struct Resolution{
-    var width = 0
-    var height = 0
+//Struct Vs. Class
+/*class Developer{
+ let name: String
+ var language: String
+ 
+ init(name: String, language: String){
+ self.name = name
+ self.language = language
+ }
+ }
+ 
+ let chacon = Developer(name: "Jeisson", language: "Swift")
+ let chacon1 = chacon // A pesar de que se creo como una constante, cómo es una clase si la deja cambiar y asignarle un nuevo valor en la linea 17
+ 
+ chacon1.language = "Java"
+ chacon.language
+ chacon1.language //Se trata del mismo objeto ya que hay dos simbolos indicando al mismo objeto
+ */
+
+struct Developer{
+    let name: String
+    var language: String
 }
 
-class VideMode{
-    var resolution = Resolution()
-    var interlased = false
-    var framRate = 0.0
-}
+let chacon = Developer(name: "Jeisson", language: "Swift")
+var chacon1 = chacon // Para poder hacer esto antes hay que crearlo como variable
 
-var defaultRes: Resolution = Resolution()
-print(defaultRes)
-let someResolution = Resolution()
-let someVideoMode = VideMode()
-print(someVideoMode.interlased)
-
-struct Size{
-    var width = 0
-    var height = 0
-}
-let s1 = Size()
-let s2 = Size()
-
-//Accessing properties
-print(s1.width)
-print(someVideoMode.resolution.height)
-someVideoMode.resolution.height = 1080
-print(someVideoMode.resolution.height)
-
-let vga = Resolution(width: 640, height: 480)
-let mySize = Size()
-print(mySize.width)
-
-let hd = Resolution(width: 1920, height: 1080)
-var cinema = hd
-print(hd)
-
-cinema.width = 2048
-
-let res = Resolution()
-print(res.width)
-
-class VideoMode {
-   var resolution = Resolution()
-   var interlaced = false
-   var frameRate = 0.0
-}
-
-let tenEighty = VideoMode()
-tenEighty.resolution = hd
-tenEighty.interlaced = true
-print(tenEighty.resolution)
-
-let alsoTenEighty = tenEighty
-alsoTenEighty.interlaced = false
-print(alsoTenEighty.interlaced)
-
-
-class Test {
-   var num = 17
-}
-let a = Test()
-let b = a
-b.num = 42
-print(a.num)
-
-if tenEighty === alsoTenEighty {
- print("Same")
-} else {
- print("Not same")
-}
+chacon1.language = "Java"
+chacon.language
+chacon1.language //Se trata objetos diferentes ya que hacen referencia a ubicaciones diferentes
