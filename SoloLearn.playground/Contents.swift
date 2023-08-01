32ab2c4022203242c0f6681d750970d6b85da4f1
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-01 14:56:56 -0500

    Quiz

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,24 +1,58 @@
 import Foundation
 
-//Deinitialization
-class SomeClass{
-    init(){
-        print("init is called")
-    }
-    deinit{
-        print("deinit is called")
+//Quiz
+class MyClass{
+    func foo(a: Int, b: Int){
+        print((a - b) / b)
     }
 }
-var s: SomeClass? = SomeClass()
-s = nil
+var mc = MyClass()
+print(mc.foo(a: 10, b: 2))
 
-//Exercise
-class Mother{
-    init(){
-        print("Init")
+class A {
+    func myprint() {
+    print("a")
     }
-    deinit{
-        print("Deinit")
+    func myprint(str:String) {
+    print("b")
+    }
+    func myprint(x:Int) {
+    print("c")
     }
 }
+var inst = A()
+inst.myprint(x:12)
 
+class Player {
+    var lives: Int=100
+    var strength: Int
+    init(lives:Int, st:Int) {
+        self.lives = lives
+        self.strength = st
+    }
+    func Attack(p:Player) {
+        p.lives -= self.strength
+    }
+}
+let p1 = Player(lives: 100, st:15)
+let p2 = Player(lives: 80, st:30)
+p2.Attack(p: p1)
+print(p1.lives)
+
+class Animal{
+    var feet: Int = 2
+    func makeNoise(){
+        print("Animal Sound")
+    }
+}
+class Dog: Animal{
+    override func makeNoise() {
+        print("Barking")
+    }
+    override init() {
+        super.init()
+        feet = 3
+    }
+}
+var d = Dog()
+print(d.feet)
