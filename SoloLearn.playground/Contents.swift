50dd46e260e0b89cadc4eb66b35608adde07dab6
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-01 10:54:31 -0500

    Subscripts

--- SoloLearn.playground/Contents.swift
+++ SoloLearn.playground/Contents.swift
@@ -1,71 +1,50 @@
 import Foundation
 
-//Methods
-class Counter{
-    var count = 0
-    func increment(){
-        //count += 1
-        self.count += 1
-    }
-    func incrementBy(amount: Int){
-        count += amount
-    }
-    func reset(){
-        count = 0
+//Subscripts
+struct TimesTable{
+    let multiplier: Int
+    subscript(index: Int) -> Int{
+        print(multiplier)
+        print(index)
+        return multiplier * index
     }
 }
-var cnt = Counter()
-cnt.reset()
-cnt.increment()
-print(cnt.count)
-cnt.incrementBy(amount: 3)
-print(cnt.count)
+let threeTimesTable = TimesTable(multiplier: 3)
+print(threeTimesTable[5])
 
-let counter = Counter()
-counter.increment()
-counter.incrementBy(amount: 5)
-counter.reset()
-print(counter.count)
-
-class Student{
-    var age = 0
-    func printAge(){
-        print(age)
+struct Matrix{
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int){
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
     }
-}
-var st = Student()
-st.printAge()
-
-//The self property
-//Line 8
-struct Point{
-    var x = 0.0, y = 0.0
-    func isToTheRight(x: Double) -> Bool{
-        return self.x > x
-    }
-    mutating func moveByX(dx: Double, dy: Double){
-        x += dx
-        y += dy
-    }
-}
-var p = Point()
-print(p.isToTheRight(x: 1.1))
-p.moveByX(dx: 2.2, dy: 2.4)
-print(p)
-//Modifying value type
-//Line 46 to 49  and 53 to 54
-struct Person{
-    var age = 0
-    mutating func addOne(){
-        age += 1
+    
+    subscript(row: Int, column: Int) -> Double{
+        get{
+            print(row)
+            print(column)
+            print(rows)
+            print(columns)
+            return grid[(row * columns) + column]
+        }
+        set{
+            grid[(row * columns) + column] = newValue
+        }
     }
 }
 
-//Type Methods
-class SomeClass{
-    static func someTypeMethod(){
-        print("I'm a type method")
+var m = Matrix(rows: 2, columns: 2)
+m[0,0] = 1.1
+m[0,1] = 2.1
+print(m[0,0])
+
+struct Test{
+    var num = 0
+    subscript(tmp: Int) -> Int {
+        return tmp*num
     }
 }
-SomeClass.someTypeMethod()
-
+var t = Test(num:8)
+print(t[2])
